22640440bc5ccc353d8613cae2564998d1f862f8
Erwin Marsi <emarsi@idi.ntnu.no>
2014-01-24 11:49:23 +0100

    case-insensitive marker in event_samp

--- funcs/event_samp.m
+++ funcs/event_samp.m
@@ -6,8 +6,12 @@ function samp_nums = event_samp(data, marker)
 % marker: string
 %   any marker (matching is only on first character and case-insensitive)
 
-samp_nums = data.events(marker);
-
+if data.events.isKey(lower(marker))
+   samp_nums = data.events(lower(marker))';
+else
+    samp_nums = data.events(upper(marker))';
+end
+    
 %%% Old method:
 % events = col(data, 'Event');
 %%% find sample numbers of events
