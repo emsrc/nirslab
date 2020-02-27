24a21e830a7c55fe2ce794c47108c6f18c29031c
Erwin Marsi <e.marsi.work@gmail.com>
2020-02-27 12:18:38 +0100

    bug fix: changes in readtable() in Matlab R2019b broke read_jaeger_tt()

--- scripts/read_jaeger_tt_directly.m
+++ scripts/read_jaeger_tt_directly.m
@@ -2,18 +2,5 @@
 
 clear;
 
-tt1 = read_jaeger_tt('../../../jaeger_works_on_windows/17hyp001_t2_m2_c1_jaeg.GDT');
-% 
-tt2 = read_jaeger_tt('../../../jaeger_works_on_windows/17mus002_t2_jaeg.GDT');
-% 
-tt3 = read_jaeger_tt('../../../jaeger_works_on_windows/17mus018_t2_jaeg.GDT');
-% 
-% 
-% 
-tt4 = read_jaeger_tt('../../../jaeger_errors/17hyp001_t2_m3_c1_jaeg.GDT');
-% 
-tt5 = read_jaeger_tt('../../../jaeger_errors/17hyp002_t2_m3_c1_jaeg.GDT');
-% 
-tt6 = read_jaeger_tt('../../../jaeger_errors/17hyp003_t2_m3_c1_jaeg.GDT');
-% 
-tt7 = read_jaeger_tt('../../../jaeger_errors/17hyp010_t2_m3_c1_jaeg.GDT');
+
+tt1 = read_jaeger_tt('../../../temp/jaeger_dsnt_work/19sar009_d2_jaeg.gdt');
